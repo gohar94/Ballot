271e7e2c1f3cc5cf04d259f9cd392b11959114ce
gohar94 <goharirfan94@gmail.com>
2016-01-16 23:09:34 +0500

    fb production variables

--- config/env/production.js
+++ config/env/production.js
@@ -38,8 +38,8 @@ module.exports = {
     }
   },
   facebook: {
-    clientID: process.env.FACEBOOK_ID || 'APP_ID',
-    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+    clientID: process.env.FACEBOOK_ID || '1048881855155480',
+    clientSecret: process.env.FACEBOOK_SECRET || 'a63b6f9ad8c4f7c29e355bc019d5d591',
     callbackURL: '/api/auth/facebook/callback'
   },
   twitter: {
