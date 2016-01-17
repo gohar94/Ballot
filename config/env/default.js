d5807298a11a793b0ab423b5487850c5f11b3bbe
gohar94 <goharirfan94@gmail.com>
2016-01-17 13:11:16 +0500

    icon, home page change

--- config/env/default.js
+++ config/env/default.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
-    googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
+    title: 'Ballot',
+    description: 'Ask. Vote. Know.',
+    keywords: 'Vote, Ballot, Ask, Information',
+    googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'UA-72523579-1'
   },
   port: process.env.PORT || 3000,
   host: process.env.HOST || '0.0.0.0',
